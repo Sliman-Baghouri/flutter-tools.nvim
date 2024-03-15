e524038f91ec58f9039c68bfa9f15dff92e365d4
Sliman-Baghouri <98884164+Sliman-Baghouri@users.noreply.github.com>
2024-03-15 01:49:01 +0100

    Update labels.lua

--- lua/flutter-tools/labels.lua
+++ lua/flutter-tools/labels.lua
@@ -15,23 +15,19 @@ local function render_labels(labels, opts)
     local highlight = opts.highlight or "Comment"
     local prefix = opts.prefix or "// "
 
-    local previous_line = -1
     for _, item in ipairs(labels) do
         local line = item.range["end"].line
-        -- Check if the label will overlap with the previous one
-        if line ~= previous_line then
-            local ok, _ = pcall(api.nvim_buf_set_extmark, 0, namespace, line, -1, {
-                virt_text = {{
-                    prefix .. item.label,
-                    highlight,
-                }},
-                virt_text_pos = "eol",
-                hl_mode = "combine",
-            })
-            -- Only update previous_line if setting extmark was successful
-            if ok then
-                previous_line = line
-            end
+        local ok, err = pcall(api.nvim_buf_set_extmark, 0, namespace, tonumber(line), -1, {
+            virt_text = {{
+                prefix .. item.label,
+                highlight,
+            }},
+            virt_text_pos = "eol",
+            hl_mode = "combine",
+        })
+        if not ok then
+            local name = api.nvim_buf_get_name(0)
+            ui.notify(fmt("Error drawing label for %s on line %d.\nBecause: %s", name, line, err), ui.ERROR)
         end
     end
 end
@@ -41,8 +37,11 @@ function M.closing_tags(err, response, _)
     if err or not opts.enabled then return end
     local uri = response.uri
     if uri ~= vim.uri_from_bufnr(0) then return end
-    -- Use pcall to handle any potential errors silently
-    pcall(render_labels, response.labels, opts)
+    render_labels(response.labels, opts)
+end
+
+function M.clear_labels()
+    api.nvim_buf_clear_namespace(0, namespace, 0, -1)
 end
 
 return M
