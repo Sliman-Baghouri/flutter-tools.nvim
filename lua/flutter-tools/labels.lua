8d4d4e5b8fad1d429dde0fc8899366094074cde2
Sliman-Baghouri <98884164+Sliman-Baghouri@users.noreply.github.com>
2024-03-15 01:30:16 +0100

    Update labels.lua

--- lua/flutter-tools/labels.lua
+++ lua/flutter-tools/labels.lua
@@ -1,6 +1,6 @@
 local lazy = require("flutter-tools.lazy")
-local ui = lazy.require("flutter-tools.ui") ---@module "flutter-tools.ui"
-local config = lazy.require("flutter-tools.config") ---@module "flutter-tools.labels"
+local ui = lazy.require("flutter-tools.ui")
+local config = lazy.require("flutter-tools.config")
 
 local api = vim.api
 local fmt = string.format
@@ -10,37 +10,39 @@ local M = {}
 local namespace = api.nvim_create_namespace("flutter_tools_closing_labels")
 
 local function render_labels(labels, opts)
-  api.nvim_buf_clear_namespace(0, namespace, 0, -1)
-  opts = opts or {}
-  local highlight = opts and opts.highlight or "Comment"
-  local prefix = opts and opts.prefix or "// "
+    api.nvim_buf_clear_namespace(0, namespace, 0, -1)
+    opts = opts or {}
+    local highlight = opts and opts.highlight or "Comment"
+    local prefix = opts and opts.prefix or "// "
 
-  for _, item in ipairs(labels) do
-    local line = item.range["end"].line
-    local ok, err = pcall(api.nvim_buf_set_extmark, 0, namespace, tonumber(line), -1, {
-      virt_text = { {
-        prefix .. item.label,
-        highlight,
-      } },
-      virt_text_pos = "eol",
-      hl_mode = "combine",
-    })
-    if not ok then
-      local name = api.nvim_buf_get_name(0)
-      ui.notify(fmt("error drawing label for %s on line %d.\nbecause: ", name, line, err), ui.ERROR)
+    local previous_line = -1
+    for _, item in ipairs(labels) do
+        local line = item.range["end"].line
+        -- Check if the label will overlap with the previous one
+        if line ~= previous_line then
+            local ok, err = pcall(api.nvim_buf_set_extmark, 0, namespace, tonumber(line), -1, {
+                virt_text = {{
+                    prefix .. item.label,
+                    highlight,
+                }},
+                virt_text_pos = "eol",
+                hl_mode = "combine",
+            })
+            if not ok then
+                local name = api.nvim_buf_get_name(0)
+                ui.notify(fmt("error drawing label for %s on line %d.\nbecause: ", name, line, err), ui.ERROR)
+            end
+            previous_line = line
+        end
     end
-  end
 end
 
---- returns a function which handles rendering floating labels
 function M.closing_tags(err, response, _)
-  local opts = config.closing_tags
-  if err or not opts.enabled then return end
-  local uri = response.uri
-  -- This check is meant to prevent stray events from over-writing labels that
-  -- don't match the current buffer.
-  if uri ~= vim.uri_from_bufnr(0) then return end
-  render_labels(response.labels, opts)
+    local opts = config.closing_tags
+    if err or not opts.enabled then return end
+    local uri = response.uri
+    if uri ~= vim.uri_from_bufnr(0) then return end
+    render_labels(response.labels, opts)
 end
 
 return M
